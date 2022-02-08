458407929d440d2caf2837fefbbcda3b65962e4e
ShravyaRNadig <47104182+ShravyaRNadig@users.noreply.github.com>
2022-02-08 09:36:51 +0530

    Create dart_application_1.dart

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,18 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+  var age = 25;
+  var name = 'Shravya';
+  var Myage = 'i am $name $age years old';
+  print(Myage);
+  // var x = {-90.abs()};
+  // var x = {90.89.ceil()};
+  var x = {90.89.floor()};
+  print(x);
+  var xis = 'the absolute is $x';
+  print(xis);
+  var multi = '''this is 
+              multi line
+              string''';
+  print(multi);
+  print(multi[2]);
+}
